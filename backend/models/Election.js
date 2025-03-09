b42bd9f5ddd72e0481344570f05c06464be74a76
DAVID <ddelcamp@ucm.es>
2025-03-09 13:48:05 +0100

    ElectionRepository y model Election

---
+++ backend/models/Election.js
@@ -0,0 +1,12 @@
+export default class Election {
+
+    constructor(id = null, title, photourl, initialDate, finalDate) {
+        this.id = id;
+        this.title = title;
+        this.photourl = photourl;
+        this.initialDate = initialDate;
+        this.finalDate = finalDate;
+       
+    }
+}
+  
---
+++ backend/repositories/ElectionRepository.js
@@ -0,0 +1,24 @@
+import BaseRepository from './BaseRepository.js';
+
+export default class UserRepository extends BaseRepository {
+
+    constructor() {
+        super();
+    }
+
+    async create(entity) {
+        throw new Error('Método create() debe ser implementado.');
+    }
+
+    async delete(id) {
+        throw new Error('Método delete() debe ser implementado.');
+    }
+
+    async findById(id) {        //buscar por entidad en vez de id?
+        throw new Error('Método findById() debe ser implementado.');
+    }
+
+    async update(entity, id) {
+        throw new Error('Método update() debe ser implementado.');
+    }
+}
