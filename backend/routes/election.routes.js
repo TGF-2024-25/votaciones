ac2eedbbdb0c3108b7f6c13711f2cefff5268a38
DAVID <ddelcamp@ucm.es>
2025-03-13 16:46:46 +0100

    Completado election.routes

--- backend/routes/election.routes.js
+++ backend/routes/election.routes.js
@@ -1,17 +1,31 @@
 import express from 'express';
 import { 
     controller_election_create, 
-    controller_election_getAll, 
-    controller_election_getById, 
-    controller_election_update, 
     controller_election_delete, 
-    controller_election_start, 
-    controller_election_close, 
-    controller_election_getCandidacies, 
-    controller_election_addCandidacy, 
-    controller_election_removeCandidacy 
+    controller_election_modify, 
+    controller_election_search, 
+    controller_election_consult, 
+    controller_election_vote, 
+    controller_election_verifyVote, 
+    controller_election_addCandidate,
+    controller_election_countVotes
 } from '../controllers/election.controller.js';
 
 const router = express.Router();
+
+//CRUD
+router.post('/createElection', controller_election_create);
+router.post('/modifyElection/:id', controller_election_modify);
+router.post('/deleteElection/:id', controller_election_delete);
+router.get('/searchElection/:id', controller_election_search);
+router.get('/consultElection/:id', controller_election_consult);
+
+router.post('/vote/:id', controller_election_vote);
+router.get('/verifyVote/:voterHashId', controller_election_verifyVote);
+router.get('/countVotes/:id', controller_election_countVotes);
+
+router.post('/addCandidate/:id', controller_election_addCandidate); // Añadir un candidato a la Election
+
+
 
 export default router;
