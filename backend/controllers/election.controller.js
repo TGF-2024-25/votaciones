98c23bde914d2987014a0867e322d5cb889c58b8
DAVID <ddelcamp@ucm.es>
2025-03-13 17:02:49 +0100

    Election Controller

---
+++ backend/controllers/election.controller.js
@@ -0,0 +1,11 @@
+import { controller_election_create, controller_election_update, controller_election_delete, controller_election_search } from '../services/election.service.js'
+
+
+
+export const controller_election_create = async (req, res) => {};
+
+export const controller_election_update = async (req, res) => {};
+
+export const controller_election_delete = async (req, res) => {};
+
+export const controller_election_search = async (req, res) => {};
