21ee65f697c8f7ec61c1d0b96603c5cae4f87dbb
DAVID <ddelcamp@ucm.es>
2025-03-25 11:34:41 +0100

    ElectionTest Frontend

---
+++ frontend/src/components/ElectionTest.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <h1>Pruebas para Elecciones</h1>
+      <button @click="probarCreate">Crear Elección</button>
+      <button @click="probarUpdate">Actualizar Elección</button>
+      <button @click="probarConsult">Consultar Elección</button>
+      <button @click="probarDelete">Eliminar Elección</button>
+    </div>
+  </template>
+  
+  <script>
+  import { testElectionCreate, testElectionUpdate, testElectionConsult, testElectionDelete } from '../test/testElections.js';
+  
+  export default {
+    methods: {
+      async probarCreate() {
+        await testElectionCreate();
+      },
+      async probarUpdate() {
+        await testElectionUpdate();
+      },
+      async probarConsult() {
+        await testElectionConsult();
+      },
+      async probarDelete() {
+        await testElectionDelete();
+      } 
+  },
+  };
+  </script>
